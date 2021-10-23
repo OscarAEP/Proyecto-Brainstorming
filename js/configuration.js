71eb51ab02ac9d0a1a0586b2890789a600e0c876
Osquitar123 <sancexrrr@gmail.com>
2021-10-22 19:08:04 -0500

    first commit

---
+++ js/configuration.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDo1F_miKm-I01SVsfYGbcpJAMdmPeXCL4",
+    authDomain: "brainstorming-75b78.firebaseapp.com",
+    projectId: "brainstorming-75b78",
+    storageBucket: "brainstorming-75b78.appspot.com",
+    messagingSenderId: "747162976171",
+    appId: "1:747162976171:web:8fa048f1ec8f421bfc9cee"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
